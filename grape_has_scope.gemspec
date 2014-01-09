d76a5b8b6d330a4d2579f2b40c2d2e2c2c71f14c
Phobos98 <phobos98@phobos98.net>
2014-01-09 10:57:48 +0200

    Initial

---
+++ grape_has_scope.gemspec
@@ -0,0 +1,23 @@
+$:.push File.expand_path("../lib", __FILE__)
+
+Gem::Specification.new do |s|
+  s.name        = "grape_has_scope"
+  s.platform    = Gem::Platform::RUBY
+  s.version     = "0.1.0"
+  s.author      = "Kourza Ivan a.k.a. Phobos98"
+  s.email       = "phobos98@phobos98.net"
+  s.homepage    = "https://github.com/Phobos98/grape_has_scope.git"
+  s.summary     = "has_scope implementation to use with Grape API"
+  s.description = %{has_scope implementation to use with Grape API}
+  s.license     = 'MIT'
+
+  s.required_rubygems_version = ">= 1.3.6"
+  s.rubyforge_project         = "grape_has_scope"
+
+  s.files = Dir['lib/*.rb']
+  s.require_path = 'lib'
+  s.autorequire = 'builder'
+  s.has_rdoc = true
+  s.extra_rdoc_files = Dir['[A-Z]*']
+
+end
---
+++ lib/grape_has_scope.rb
@@ -0,0 +1,150 @@
+module GrapeHasScope
+  TRUE_VALUES = ["true", true, "1", 1]
+
+  ALLOWED_TYPES = {
+    :array   => [ Array ],
+    :hash    => [ Hash ],
+    :boolean => [ Object ],
+    :default => [ String, Numeric ]
+  }
+
+  def self.included(base)
+    base.class_eval do
+      class_attribute :scopes_configuration
+    end
+  end
+
+  def has_scope(*scopes, &block)
+    options = scopes.extract_options!
+    options.symbolize_keys!
+    options.assert_valid_keys(:type, :only, :except, :if, :unless, :default, :as, :using, :allow_blank)
+
+    if options.key?(:using)
+      if options.key?(:type) && options[:type] != :hash
+        raise "You cannot use :using with another :type different than :hash"
+      else
+        options[:type] = :hash
+      end
+
+      options[:using] = Array(options[:using])
+    end
+
+    options[:only]   = Array(options[:only])
+    options[:except] = Array(options[:except])
+
+    self.class.scopes_configuration = (self.class.scopes_configuration || {}).dup
+
+    scopes.each do |scope|
+      self.class.scopes_configuration[scope] ||= { :as => scope, :type => :default, :block => block }
+      self.class.scopes_configuration[scope] = self.class.scopes_configuration[scope].merge(options)
+    end
+  end
+
+  # Receives an object where scopes will be applied to.
+  #
+  #   class GraduationsController < InheritedResources::Base
+  #     has_scope :featured, :type => true, :only => :index
+  #     has_scope :by_degree, :only => :index
+  #
+  #     def index
+  #       @graduations = apply_scopes(Graduation).all
+  #     end
+  #   end
+  #
+  def apply_scopes(target, hash=params)
+    return target unless scopes_configuration
+
+    self.class.scopes_configuration.each do |scope, options|
+      next unless apply_scope_to_action?(options)
+      key = options[:as]
+
+      if hash.key?(key)
+        value, call_scope = hash[key], true
+      elsif options.key?(:default)
+        value, call_scope = options[:default], true
+        value = value.call(self) if value.is_a?(Proc)
+      end
+
+      value = parse_value(options[:type], key, value)
+      value = normalize_blanks(value)
+
+      if call_scope && (value.present? || options[:allow_blank])
+        current_scopes[key] = value
+        target = call_scope_by_type(options[:type], scope, target, value, options)
+      end
+    end
+
+    target
+  end
+
+  # Set the real value for the current scope if type check.
+  def parse_value(type, key, value) #:nodoc:
+    if type == :boolean
+      TRUE_VALUES.include?(value)
+    elsif value && ALLOWED_TYPES[type].any?{ |klass| value.is_a?(klass) }
+      value
+    end
+  end
+
+  # Screens pseudo-blank params.
+  def normalize_blanks(value) #:nodoc:
+    return value if value.nil?
+    if value.is_a?(Array)
+      value.select { |v| v.present? }
+    elsif value.is_a?(Hash)
+      value.select { |k, v| normalize_blanks(v).present? }.with_indifferent_access
+    else
+      value
+    end
+  end
+
+  # Call the scope taking into account its type.
+  def call_scope_by_type(type, scope, target, value, options) #:nodoc:
+    block = options[:block]
+
+    if type == :boolean
+      block ? block.call(self, target) : target.send(scope)
+    elsif value && options.key?(:using)
+      value = value.values_at(*options[:using])
+      block ? block.call(self, target, value) : target.send(scope, *value)
+    else
+      block ? block.call(self, target, value) : target.send(scope, value)
+    end
+  end
+
+  # Given an options with :only and :except arrays, check if the scope
+  # can be performed in the current action.
+  def apply_scope_to_action?(options) #:nodoc:
+    return false unless applicable?(options[:if], true) && applicable?(options[:unless], false)
+
+    if options[:only].empty?
+      options[:except].empty? || !options[:except].include?(action_name.to_sym)
+    else
+      options[:only].include?(action_name.to_sym)
+    end
+  end
+
+  # Evaluates the scope options :if or :unless. Returns true if the proc
+  # method, or string evals to the expected value.
+  def applicable?(string_proc_or_symbol, expected) #:nodoc:
+    case string_proc_or_symbol
+      when String
+        eval(string_proc_or_symbol) == expected
+      when Proc
+        string_proc_or_symbol.call(self) == expected
+      when Symbol
+        send(string_proc_or_symbol) == expected
+      else
+        true
+    end
+  end
+
+  # Returns the scopes used in this action.
+  def current_scopes
+    @current_scopes ||= {}
+  end
+end
+
+class Grape::Endpoint
+  include GrapeHasScope
+end
